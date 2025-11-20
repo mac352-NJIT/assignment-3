a75bf2a2b0e5b4c0a068ffcc198beb44d207698c
Manuel Calle <mac352@njit.edu>
2025-11-20 01:23:13 -0500

    Finished assignment

--- index.js
+++ index.js
@@ -8,22 +8,21 @@ function countTotalVowelsAndConsonants() {
     }
 
     const reader = new FileReader();
-    reader.onload = function(files) {
-        const textFile = files.target.result;
+
+    reader.onload = function(e) {
+        const textFile = e.target.result;
         
         fetch('/text-assignment', {
             method: 'POST',
-            headers: {'Content-Type': "application/json"},
+            headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify({ text: textFile })
 
         })
-
         .then(response => response.json())
         .then(data => {
-            document.getElementByID("totalVowels").textContent = data.totalVowels;
-            document.getElementByID("totalConsonants").textContent = data.totalConsonants;
+            document.getElementById('totalVowels').textContent = data.totalVowels;
+            document.getElementById('totalConsonants').textContent = data.totalConsonants;
         })
-
         .catch(error => {
             console.error('Error: ', error);
         });
@@ -31,4 +30,29 @@ function countTotalVowelsAndConsonants() {
 
     reader.readAsText(file);
 
+}
+
+function calculateBMI() {
+    const weight = parseFloat(document.getElementById('inputWeight').value);
+    const heightFeet = parseInt(document.getElementById('inputHeightFeet').value);
+    const heightInches = parseInt(document.getElementById('inputHeightInches').value);
+
+    if (!weight || !heightFeet || !heightInches) {
+        window.alert("Please fill in all fields to continue");
+        return;
     }
+
+    fetch('/bmi-calculator', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({weight, heightFeet, heightInches}) 
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('bmiResult').textContent = data.bmi;
+    })
+    .catch(error => {
+        console.error('Error: ', error);
+    });
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname));
 app.use(express.json());
 
 app.post('/text-assignment', (req, res) => {
@@ -8,25 +9,53 @@ app.post('/text-assignment', (req, res) => {
     let vowelsCount = 0;
     let consonantsCount = 0;
     let len = text.length;
+    const vowels = /[aeiou]/i;
+    const consonants = /[bcdfghjklmnpqrstvwxyz]/i;
 
     for (let i = 0; i < len; i++) {
-        if (text[i].match(/[aeiou]/i)) {
+
+        const char = text[i];
+        
+
+
+        if (vowels.test(char)) {
             vowelsCount++;
         }
-        else if (text[i].match(/[bcdfghjklmnpqrstvwxyz]/i)) {
+        else if (consonants.test(char)) {
             consonantsCount++;
         }
     }
 
     res.json({ totalVowels: vowelsCount, totalConsonants: consonantsCount });
-})
+});
+
+app.post('/bmi-calculator', (req, res) => {
+    const { weight, heightFeet, heightInches } = req.body;
+    let w = parseFloat(weight);
+    let hft = parseInt(heightFeet);
+    let hin = parseInt(heightInches);
+
+    if (!w || !hft || !hin) {
+        res.status(400).json({ error: "Missing required parameters" });
+        return;
+    }
+
+    const heightInInches = (hft * 12) + parseInt(hin);
+    const heightInMeters = heightInInches * 0.0254;
+    const weightInKilograms = w * 0.453592;
+    const bmi = weightInKilograms / (heightInMeters * heightInMeters);
+
+    res.json({ bmi: bmi.toFixed(2)});
+    
+});
 
 const port = 3000;
 
 app.get('/text-assignment', (req, res) => {
     res.send('This is the text assignment endpoint.');
-})
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000'`);
 });
 
